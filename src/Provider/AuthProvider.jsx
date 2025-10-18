a7682c4ef6fb3b95dc5af0c728fe3b03be1e7a52
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-10-19 02:39:50 +0600

    check the logged in user onAUth....

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,12 +1,27 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 import React from "react";
 import { useState } from "react";
 import { createContext } from "react";
 import auth from "../FireBase/firebase.config.js";
+import { useEffect } from "react";
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  console.log(user);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
