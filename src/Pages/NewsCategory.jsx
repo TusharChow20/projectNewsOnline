2da4e149f37ee42659457aa53e9fec3cc4c01a9d
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-10-18 14:06:55 +0600

    pick breaking news

--- src/Pages/NewsCategory.jsx
+++ src/Pages/NewsCategory.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { useEffect } from "react";
 import { useLoaderData, useParams } from "react-router";
 
@@ -6,17 +7,29 @@ const NewsCategory = () => {
   const { id } = useParams();
   const { data } = useLoaderData();
   //   console.log(data);
+  const [news, setNews] = useState([]);
 
   useEffect(() => {
-    const filteredData = data.filter(
+    if(id === '0'){
+        setNews(data)
+    }
+    else if (id ==='1'){
+        const filteredData = data.filter(
+      (eachData) => eachData.others.is_today_pick === true
+    );
+     setNews(filteredData);
+    }
+    else{
+        const filteredData = data.filter(
       (eachData) => eachData.category_id === parseInt(id)
     );
-    console.log(filteredData);
+    setNews(filteredData);
+    }
   }, [data, id]);
 
   return (
     <div>
-      <h1>This is navbaR</h1>
+      <h1>Total News : {news.length}</h1>
     </div>
   );
 };
